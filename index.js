a366e2daae8df6f0fc0f108b5af6525ddf456b99
Andres Garcia <oagarciar@gmail.com>
2016-02-16 16:58:25 -0500

    tweaks

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var divPoint1 = document.createElement('div');
 var divPoint2 = document.createElement('div');
 var line = document.createElement('div');
 
+divPoint1.classList.add('point');
+divPoint2.classList.add('point');
+line.classList.add('line');
+
 //Draw the elements in the div
 function draw() {
   divPoint1.style.left = pointA.x + 'px';
@@ -24,6 +28,7 @@ function draw() {
   line.style.width = pitagoras() + 'px';
 }
 
+//Get 2 points angle based on atan2
 function calculateAngle() {
   //var output = theta;
   var output = Math.atan2(pointB.y - pointA.y, pointB.x - pointA.x) * 180 / Math.PI;
@@ -31,26 +36,25 @@ function calculateAngle() {
   return output;
 }
 
+//Calculate distance between 2 points
 function pitagoras() {
   //return Math.hypot(pointB.x - pointA.x, pointB.y - pointA.y);
   return Math.sqrt( Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointB.y, 2));
 }
 
+//To generate aleatory points
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-divPoint1.classList.add('point');
-divPoint2.classList.add('point');
-line.classList.add('line');
-
-//Print
-canvas.appendChild(line);
-canvas.appendChild(divPoint1);
-canvas.appendChild(divPoint2);
-
+//Draw each 1s
 window.setInterval(function() {
   pointA = {x: getRandomArbitrary(0, 650), y:getRandomArbitrary(0, 450)};
   pointB = {x: getRandomArbitrary(0, 650), y:getRandomArbitrary(0, 450)};
   draw();
 }, 1000);
+
+//Add elements to div container
+canvas.appendChild(line);
+canvas.appendChild(divPoint1);
+canvas.appendChild(divPoint2);
